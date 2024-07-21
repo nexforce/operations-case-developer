d585870ce630c468162feb59f40550a1ef7e999e
Ordeeper <pedrordeeper@gmail.com>
2024-07-21 11:11:42 -0300

    feat(backend): Add necessary routes

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-    res.send("Homepage")
+/* GET Products */
+router.get("/products", function(req, res) {
+   res.json("Return all products");
+});
+
+/* GET Product */
+router.get("/products/:productId", function(req, res) {
+   res.json("Return single product");
+});
+
+/* DELETE Product */
+router.delete("/products/:productId", function(req, res) {
+   res.json("delete product");
+});
+
+/* POST Product */
+router.post("/products", function(req, res) {
+   res.json("create product");
+});
+
+/* PATCH Product */
+router.patch("/products/:prodctId", function(req, res) {
+   res.json("update product");
 });
 
 module.exports = router;
