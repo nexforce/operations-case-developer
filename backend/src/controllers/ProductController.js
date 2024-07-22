a5923eff22cf5e87a18675d000c7141933b67837
Ordeeper <pedrordeeper@gmail.com>
2024-07-22 16:56:10 -0300

    feat(backend): Add controllers and update routes

---
+++ backend/src/controllers/ProductController.js
@@ -0,0 +1,63 @@
+const { validationResult } = require("express-validator");
+const ProductService = require("../services/ProductService.js");
+
+const productService = new ProductService();
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await productService.getAllProducts();
+        res.json(products);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    const productId = req.params.productId;
+    try {
+        const product = await productService.getProductById(productId);
+        res.json(product);
+    } catch (error) {
+        res.status(404).send(error.message); // Not Found
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    const productId = req.params.productId;
+    try {
+        const deletedProduct = await productService.deleteProduct(productId);
+        res.json(deletedProduct);
+    } catch (error) {
+        res.status(404).send(error.message); // Not Found
+    }
+};
+
+exports.createProduct = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // If there are validation errors, return them in the response
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const product = await productService.createProduct(req.body);
+        res.status(201).json(product); // 201 Created
+    } catch (error) {
+        res.status(400).send(error.message); // Bad Request
+    }
+};
+
+exports.updateProduct = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // If there are validation errors, return them in the response
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const productId = req.params.productId;
+    try {
+        const updatedProduct = await productService.updateProduct(productId, req.body);
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(400).send(error.message); // Bad Request
+    }
+};
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,29 +1,78 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+
+const ProductController = require("../controllers/ProductController.js");
 
 /* GET Products */
-router.get("/products", function(req, res) {
-   res.json("Return all products");
-});
+router.get("/products", ProductController.getAllProducts);
 
 /* GET Product */
-router.get("/products/:productId", function(req, res) {
-   res.json("Return single product");
-});
+router.get("/products/:productId", ProductController.getProductById);
 
 /* DELETE Product */
-router.delete("/products/:productId", function(req, res) {
-   res.json("delete product");
-});
+router.delete("/products/:productId", ProductController.deleteProduct);
 
 /* POST Product */
-router.post("/products", function(req, res) {
-   res.json("create product");
-});
+router.post(
+    "/products",
+    [
+        body("title").isString().withMessage("Title must be a string").escape(),
+        body("price").isNumeric().withMessage("Price must be a number"),
+        body("stock").isNumeric().withMessage("Stock must be a number"),
+        body("description").isString().withMessage("Description must be a string").escape(),
+        body("categories").isArray().withMessage("Categories must be a array of string"),
+        body("image").isURL().withMessage("Image URL must be a valid URL").custom((url) => {
+            const imageExtensions = ["jpg", "jpeg", "png"];
+            const extension = url.split(".").pop().toLowerCase();
+
+            if (!imageExtensions.includes(extension)) {
+                throw new Error("Invalid image format");
+            }
+
+            return true;
+        }),
+        body("rating.rate").isNumeric().withMessage("Rate must be a number"),
+        body("rating.count").isNumeric().withMessage("Count must be a number"),
+        body("rating").custom((value) => {
+            if (!value.rate || !value.count) {
+                return false;
+            }
+            return true;
+        }),
+    ],
+    ProductController.createProduct
+);
 
 /* PATCH Product */
-router.patch("/products/:prodctId", function(req, res) {
-   res.json("update product");
-});
+router.patch(
+    "/products/:productId",
+    [
+        body("title").optional().isString().withMessage("Title must be a string").escape(),
+        body("price").optional().isNumeric().withMessage("Price must be a number"),
+        body("stock").optional().isNumeric().withMessage("Stock must be a number"),
+        body("description").optional().isString().withMessage("Description must be a string").escape(),
+        body("categories").optional().isArray().withMessage("Categories must be an array of strings"),
+        body("image").optional().isURL().withMessage("Image URL must be a valid URL").custom((url) => {
+            const imageExtensions = ["jpg", "jpeg", "png"];
+            const extension = url.split(".").pop().toLowerCase();
+
+            if (!imageExtensions.includes(extension)) {
+                throw new Error("Invalid image format");
+            }
+
+            return true;
+        }),
+        body("rating.rate").optional().isNumeric().withMessage("Rate must be a number"),
+        body("rating.count").optional().isNumeric().withMessage("Count must be a number"),
+        body("rating").optional().custom((value) => {
+            if (!value.rate || !value.count) {
+                return false;
+            }
+            return true;
+        }),
+    ],
+    ProductController.updateProduct
+);
 
 module.exports = router;
