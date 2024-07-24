9634e09a2ab10b8694afc1fd8211b0f9e33dbb52
Ordeeper <pedrordeeper@gmail.com>
2024-07-24 19:26:44 -0300

    refactor(backend): Update db connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL);
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  dialect: "postgres",
+});
 
 module.exports = sequelize;
+
