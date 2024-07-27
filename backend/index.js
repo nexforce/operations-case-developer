fb935d2a20a9324d0789dd5d0ee50b9b46a94621
LaraYasmin <lyasminoliveira18@gmail.com>
2024-07-27 16:24:29 -0300

    Organizando as pastas e estruturação do backend, primeiros passos do projeto

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
+import express from 'express';
+import router from './routes/routes';
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+app.get('/', router);
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 })
