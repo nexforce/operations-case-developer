fcae41a8dc3d921cea8035613a4918d9c760a48e
LaraYasmin <lyasminoliveira18@gmail.com>
2024-07-28 14:12:38 -0300

    Atualização de componente principal (tabela) + conexão via service/useEffect com o backend

--- frontend/src/components/tableComponent.jsx
+++ frontend/src/components/tableComponent.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getProducts } from '../utils/apiService';
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 const TableComponent = () => {
-    const [name, setName] = useState('');
-    const [category, setCategory] = useState('');
-    const [price, setPrice] = useState('');
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await getProducts();
+            const products = response.hardware || [];
+            if (Array.isArray(products)) {
+                setData(products);
+            } else {
+                console.error('Missing or invalid hardware data', products);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <div className="flex min-h-screen items-center justify-center">
@@ -11,40 +29,34 @@ const TableComponent = () => {
                 <table className="min-w-full bg-white shadow-md rounded-xl">
                     <thead>
                         <tr className="bg-blue-gray-100 text-gray-700">
-                        <th className="py-3 px-4 text-left">Nome</th>
-                        <th className="py-3 px-4 text-left">Categoria</th>
-                        <th className="py-3 px-4 text-left">Preço</th>
-                        <th className="py-3 px-4 text-left">Ações</th>
+                            <th className="py-3 px-4 text-left">Nome</th>
+                            <th className="py-3 px-4 text-left">Categoria</th>
+                            <th className="py-3 px-4 text-left">Preço</th>
+                            <th className="py-3 px-4 text-left">Ações</th>
                         </tr>
                     </thead>
                     <tbody className="text-blue-gray-900">
-                        <tr className="border-b border-blue-gray-200">
-                        <td className="py-3 px-4">Company A</td>
-                        <td className="py-3 px-4">$50.25</td>
-                        <td className="py-3 px-4">100</td>
-                        <td className="py-3 px-4">
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Editar </a>
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Deletar</a>
-                        </td>
-                        </tr>
-                        <tr className="border-b border-blue-gray-200">
-                        <td className="py-3 px-4">Company B</td>
-                        <td className="py-3 px-4">$75.60</td>
-                        <td className="py-3 px-4">150</td>
-                        <td className="py-3 px-4">
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Editar </a>
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Deletar</a>
-                        </td>
-                        </tr>
-                        <tr className="border-b border-blue-gray-200">
-                        <td className="py-3 px-4">Company C</td>
-                        <td className="py-3 px-4">$30.80</td>
-                        <td className="py-3 px-4">200</td>
-                        <td className="py-3 px-4">
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Editar </a>
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-800">Deletar</a>
-                        </td>
-                        </tr>
+                        {data.map((product, index) => (
+                            <tr key={index} className="border-b border-blue-gray-200">
+                                <td className="py-3 px-4">{product.name}</td>
+                                <td className="py-3 px-4">{product.category}</td>
+                                <td className="py-3 px-4">{product.price}</td>
+                                <td className="py-3 px-4">
+                                    <button
+                                        className="text-black-600 hover:text-black-800"
+                                        aria-label="Editar"
+                                    >
+                                        <PencilIcon className="h-5 w-5" />
+                                    </button>
+                                    <button
+                                        className="text-black-600 hover:text-black-800"
+                                        aria-label="Deletar"
+                                    >
+                                        <TrashIcon className="h-5 w-5" />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
