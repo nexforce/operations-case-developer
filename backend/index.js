0dd961a4983b6697420cc818a892f898269b7e74
LaraYasmin <lyasminoliveira18@gmail.com>
2024-07-28 14:11:28 -0300

    Atualização de cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import router from './routes/routes.js';
 import connectDB from './connectDB/connectDB.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 //connection to database
 connectDB();
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 
 app.use(express.json());
 app.use('/hardware/', router);
