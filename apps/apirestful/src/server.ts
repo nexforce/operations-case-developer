083e2df06f25f667b23cb74ed4752ebd64d38de5
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-01 08:59:47 -0300

    chore: setup routes for ExpressJS

--- apps/apirestful/src/server.ts
+++ apps/apirestful/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import routes from './config/routes'
 
 const app = express()
+
+app.use(routes)
 
 const SERVER_PORT = 5000
 
 app.listen(SERVER_PORT, () => {
-  console.log('Running server on ${SERVER_PORT}')
+  console.log(`Running server on ${SERVER_PORT}`)
 })
