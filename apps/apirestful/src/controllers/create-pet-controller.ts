7eaa04503ca96568d9c8e1be5ff8293047c72a43
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-01 16:31:19 -0300

    feat: ensure pet is insert into database

--- apps/apirestful/src/controllers/create-pet-controller.ts
+++ apps/apirestful/src/controllers/create-pet-controller.ts
@@ -1,19 +1,52 @@
 import {Request, Response} from 'express'
 import hubspotClient from '../services/hubspot-client'
+import { db } from '../services/db/client'
+import { AssociationSpecAssociationCategoryEnum } from '@hubspot/api-client/lib/codegen/crm/deals/models/AssociationSpec'
 
 class CreatePetController {
   async handle(request: Request, response: Response) {
-    const { name, breed, age } = request.body
+    const name = request.body['name'] as string
+    const age = request.body['age'] as number
+    const breed = request.body['breed'] as string
 
-    const properties = {
-      name, breed, age
-    }
+    const properties = { name, age, breed }
+
+    const associationCategory = AssociationSpecAssociationCategoryEnum.UserDefined
 
     const { results } = await hubspotClient.crm.objects.basicApi.create('pets', {
-      properties: properties
+      properties: {
+        name,
+        age: age.toString(),
+        breed,
+      },
+      associations: [
+        {
+          to: {
+            id: 'contact_id'
+          },
+          types: [
+            {
+              associationCategory: associationCategory,
+              associationTypeId : 159
+              // Create a type id for this association
+              // Link: https://legacydocs.hubspot.com/docs/methods/crm-associations/crm-associations-overview
+            }
+          ]
+        }
+      ]
     })
 
-    response.status(201).json(results)
+    const pet = await db.pet.create({
+      data: {
+        name,
+        age,
+        breedId: 'breed',
+        hubSpotId: 'id',
+        contactId: 'contactId'
+      }
+    })
+
+    return response.status(201).json(pet)
   }
 }
 
---
+++ apps/apirestful/src/services/db/client.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+
+export const db = new PrismaClient({
+  log: [
+        'query',
+        'info',
+        'warn',
+        'error'
+      ]
+})
