a4c636d7e6de4d01375b1622368d86ff123003c0
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-02 18:35:53 +0000

    chore: setup routes for update and delete pet

--- apps/apirestful/src/config/routes.ts
+++ apps/apirestful/src/config/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import CreatePetController from '../controllers/create-pet-controller'
 import ListPetsController from '../controllers/list-pets-controller'
+import UpdatePetController from '../controllers/update-pet-controller'
+import DeletePetController from '../controllers/delete-pet-controller'
 
 const router = Router()
 
@@ -15,5 +17,13 @@ router.post('/pet', createPetController.handle)
 const listPetsController = new ListPetsController()
 
 router.get('/pet', listPetsController.handle)
+
+const updatePetController = new UpdatePetController()
+
+router.put('/pet/:id', updatePetController.handle)
+
+const deletePetController = new DeletePetController()
+
+router.delete('/pet/:id', deletePetController.handle)
 
 export default router
