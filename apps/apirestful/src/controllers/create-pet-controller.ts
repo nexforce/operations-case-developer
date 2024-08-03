117445515be21c0655aeebbc150b9e8144576939
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-03 00:07:54 -0300

    refactor: break line in if statements

--- apps/apirestful/src/controllers/create-pet-controller.ts
+++ apps/apirestful/src/controllers/create-pet-controller.ts
@@ -16,17 +16,23 @@ class CreatePetController {
       })
     }
 
-    if (!age) return response.status(400).send({
-      message: 'Age is not provided. Ensure a value greater than 0 is provided.'
-    })
+    if (!age) {
+      return response.status(400).send({
+        message: 'Age is not provided. Ensure a value greater than 0 is provided.'
+      })
+    }
 
-    if (!breed) return response.status(400).send({
-      message: 'BreedId is not provided. Ensure a value of type uuid is provided.'
-    })
+    if (!breed) {
+      return response.status(400).send({
+        message: 'BreedId is not provided. Ensure a value of type uuid is provided.'
+      })
+    }
 
-    if (!contactId) return response.status(400).send({
-      message: 'ContactId is not provided. Ensure a value of type integer is provided. Read more about Contacts from HubSpot'
-    })
+    if (!contactId) {
+      return response.status(400).send({
+        message: 'ContactId is not provided. Ensure a value of type integer is provided. Read more about Contacts from HubSpot'
+      })
+    }
 
     const properties = { name, age: age.toString(), breed }
 
