6b585641eefc7a5dbf6728c2e039c196d2ff5655
suetham <matheus.oliveira.s@protonmail.com>
2024-08-04 13:46:06 -0300

    chore: ensure Eslint use set of rules for TypeScript code

--- apps/apirestful/eslint.config.mjs
+++ apps/apirestful/eslint.config.mjs
@@ -1,11 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginN from 'eslint-plugin-n'
+import pluginPromise from 'eslint-plugin-promise'
+import pluginImport from 'eslint-plugin-import'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  ...tseslint.configs.strict,
+  {
+    plugins: {
+      n: pluginN,
+      import: pluginImport,
+      promise: pluginPromise
+    }
+  },
+  {
+    ignores: ['coverage/', 'dist/', 'lib/']
+  }
+]
