3a1899e4a0a4d51030fc60a72632169354c8d322
suetham <matheus.oliveira.s@protonmail.com>
2024-08-04 21:56:57 -0300

    feat: add a button to access pets page in home page

--- apps/spa/src/pages/Home.tsx
+++ apps/spa/src/pages/Home.tsx
@@ -12,14 +12,20 @@ const Home: FC = () => {
 
         <main className="text-center pt-20 w-full flex flex-col gap-y-4">
           <div>
-            Esse é o teste prático de Matheus Oliveira para a Nexforce.
+            Esse é o teste prático de <span className="font-semibold">Matheus Oliveira</span> para a Nexforce.
           </div>
 
           <div>Accese o cabeçalho de navegação acima para utilizar a aplicação.</div>
 
-          <div className="flex gap-x-1 mx-auto pt-12">
+          <div className="flex gap-x-1 mx-auto pt-8">
             <div className="font-semibold">Tema:</div>
             <div>Pets</div>
+          </div>
+
+          <div>
+            <Button variant="outline">
+              <a href="/pets">Acessar</a>
+            </Button>
           </div>
         </main>
       </div>
